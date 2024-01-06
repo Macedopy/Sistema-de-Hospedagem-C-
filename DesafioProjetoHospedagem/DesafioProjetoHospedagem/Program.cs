016b0361a290e1a69493cf938d7866e8605c8110
Macedo <108029211+Macedopy@users.noreply.github.com>
2024-01-06 16:12:07 -0300

    Update Program.cs

--- DesafioProjetoHospedagem/DesafioProjetoHospedagem/Program.cs
+++ DesafioProjetoHospedagem/DesafioProjetoHospedagem/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using DesafioProjetoHospedagem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+List<Pessoa> hospede = new List<Pessoa>();
+
+
+Pessoa p1 = new Pessoa(nomes:"Hóspede 1");
+
+Pessoa p2 = new Pessoa(nomes:"Hóspede 2");
+
+Pessoa p3 = new Pessoa(nomes:"Hóspede 3");
+
+hospede.Add(p1);
+hospede.Add(p2);
+hospede.Add(p3);
+
+// Cria a suite
+
+Suite suite = new Suite(tiposuite: "Premium", capacidade: 3, valordiaria: 30);
+
+// Cria uma nova reserva, passando a suite e os hóspedes
+
+Reserva reserva = new Reserva(diasReservados: 12);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospede);
+
+//Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Hóspedes na reserva: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária da reserva: {reserva.CalcularValorDiaria()}");
